e0513a900423a07415c140e40aa2c0f28ee7600f
Henrique Rodrigues <96885946+Equiel-1703@users.noreply.github.com>
2023-07-08 17:05:19 -0300

    abrindo arquivos

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        eprintln!("Você deve inserir um path para um arquivo de entrada!");
+        exit(exitcode::USAGE);
+    }
+
+    // Pega o argumento
+    let filepath = &args[1];
+
+    println!("Arquivo: {}", filepath);
+    let mut f = File::open(filepath).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
